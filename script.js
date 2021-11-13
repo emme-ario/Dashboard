9d2523d2183a77798bf4320e187f9cd383fae063
emme-ario <emme-ario@users.noreply.github.com>
2021-11-13 09:21:53 +0100

    Toggle lights on/off changing style and img src

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ fetch("https://hf3xzw.deta.dev/")
 
         document.getElementById("popup").innerHTML = `
                                                     <div class="popup-container">
-                                                    <h2>${sensors[i].description}</h2>
-                                                    <p>Position:</p>
-                                                    <p>${sensors[i].position.lat}</p>
-                                                    <p>${sensors[i].position.lng}</p>
-                                                    <p>${sensors[i].position.place}<p>
-                                                    <p>${sensors[i].position.state_code}</p>
+                                                        <h2>${sensors[i].description}</h2>
+                                                        <p>Position:</p>
+                                                        <p>${sensors[i].position.lat}</p>
+                                                        <p>${sensors[i].position.lng}</p>
+                                                        <p>${sensors[i].position.place}<p>
+                                                        <p>${sensors[i].position.state_code}</p>
+                                                        <h4>Value:</h4>
+                                                        <p>${sensors[i].value}</p>
                                                     </div>
                                                     `
     }
@@ -34,11 +36,30 @@ function togglePopup() {
     popup.classList.toggle("show")
 }
 
-function switchLight() {
-    const lightSwitch = document.getElementById("light-switch")
-    lightSwitch.classList.toggle("switch")
-    console.log("switch :D")
+const lightOff = "https://img.icons8.com/ios/50/000000/light.png"
+const lightOn = "https://img.icons8.com/ios/50/000000/light-on.png"
+
+function switchLight1() {
+    const lightSwitch1 = document.getElementById("light-switch1")
+    lightSwitch1.classList.toggle("switch1")    
+
+    if(document.getElementById("light-img1").src.match(lightOff))
+        document.getElementById("light-img1").src = lightOn
+    else
+        document.getElementById("light-img1").src = lightOff
 }
+
+function switchLight2() {
+    const lightSwitch2 = document.getElementById("light-switch2")
+    lightSwitch2.classList.toggle("switch2")
+
+    if(document.getElementById("light-img2").src.match(lightOff))
+        document.getElementById("light-img2").src = lightOn
+    else
+        document.getElementById("light-img2").src = lightOff
+}
+
+
 
 
 

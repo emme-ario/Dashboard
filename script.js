2bc751047c799c25de606a99e5fbc2ea57ede419
emme-ario <emme-ario@users.noreply.github.com>
2021-11-17 00:46:36 +0100

    Adds button to close popup

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ fetch("https://hf3xzw.deta.dev/")
         sensors.push(JSONToSensor(element))
     })
 
+    // Manages Light1 Sensor
+    document.getElementById("light1").onclick = () => {
+        switchLight1()
+    }
+    
+    // Manages Light2 Sensor
+    document.getElementById("light2").onclick = () => {
+        switchLight2()
+    }
+    
+    // Fills the pop-up with sensors data
     for(let i = 0; i < 8; i++) {
-        // console.log(sensors[i])
-        
         document.getElementById("details" + (i+1)).onclick = () => {
+            togglePopup()
             document.getElementById("popup").innerHTML = `
+                                                        <a id="close-btn" class="close-btn" onclick="togglePopup()"><img src="https://img.icons8.com/material/24/000000/multiply--v1.png"/></a>
                                                         <div class="popup-container">
                                                             <h2>${sensors[i].description}</h2>
                                                             <p>Position:</p>
@@ -25,7 +36,6 @@ fetch("https://hf3xzw.deta.dev/")
                                                             <p>${sensors[i].value}</p>
                                                         </div>
                                                         `
-            togglePopup()
         }
     }
 })
@@ -43,17 +53,21 @@ const lightOff = "https://img.icons8.com/ios/50/000000/light.png"
 const lightOn = "https://img.icons8.com/ios/50/000000/light-on.png"
 
 function switchLight1() {
-    const lightSwitch1 = document.getElementById("light-switch1")
+    const lightSwitch1 = document.getElementById("light1")
     lightSwitch1.classList.toggle("switch1")    
 
-    if(document.getElementById("light-img1").src.match(lightOff))
+    if(document.getElementById("light-img1").src.match(lightOff)) {
         document.getElementById("light-img1").src = lightOn
-    else
+        //sensors[0].value = true // NOT WORKING
+    }
+    else {
         document.getElementById("light-img1").src = lightOff
+        //sensors[0].value = false // NOT WORKING
+    }
 }
 
 function switchLight2() {
-    const lightSwitch2 = document.getElementById("light-switch2")
+    const lightSwitch2 = document.getElementById("light2")
     lightSwitch2.classList.toggle("switch2")
 
     if(document.getElementById("light-img2").src.match(lightOff))
